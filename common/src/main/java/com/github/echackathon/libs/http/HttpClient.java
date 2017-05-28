9defa33eb39fd2b4c9c884fa458b14112e3cf6ee
loveapple <hao0323@gmail.com>
2017-05-28 10:24:58 +0900

    パッケージの説明を追加

---
+++ common/src/main/java/com/github/echackathon/libs/http/HttpClient.java
@@ -0,0 +1,9 @@
+package com.github.echackathon.libs.http;
+
+
+/**
+ * HTTPクライアント
+ *
+ */
+public class HttpClient{
+}
---
+++ common/src/main/java/com/github/echackathon/libs/http/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * package info
+ *
+ */
+package com.github.echackathon.libs.http;
