0b2cc3b10395cd71cdf619c822d298dc8d492542
loveapple <hao0323@gmail.com>
2017-06-22 00:36:17 +0900

    add client source

--- common/src/main/java/Test.java
+++ common/src/main/java/Test.java
@@ -1,1 +1,32 @@
-public class Test{}
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+public class Test{
+
+
+	public static void main(String args[]) {
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		HttpGet httpGet = new HttpGet("https://aw-pluralism.github.io/ec-hackathon/http_client_test/");
+		CloseableHttpResponse response = null;
+		try {
+
+			response = httpClient.execute(httpGet);
+			HttpEntity entity = response.getEntity();
+			System.out.println(entity.getContent());
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				response.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
--- common/src/main/java/com/github/echackathon/libs/http/HttpClient.java
+++ common/src/main/java/com/github/echackathon/libs/http/HttpClient.java
@@ -1,9 +1,47 @@
 package com.github.echackathon.libs.http;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 
 /**
- * HTTPクライアント
+ *
  *
  */
-public class HttpClient{
+public class HttpClient {
+
+	public static void main(String args[]) {
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		HttpGet httpGet = new HttpGet("https://aw-pluralism.github.io/ec-hackathon/http_client_test/");
+		CloseableHttpResponse response = null;
+		try {
+
+			response = httpClient.execute(httpGet);
+			HttpEntity entity = response.getEntity();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(entity.getContent(), "UTF-8"));
+			String line = null;
+			System.out.println("content start: ");
+			while ((line = reader.readLine()) != null) {
+				System.out.println(line);
+				System.out.flush();
+			}
+			System.out.println("content end: ");
+			reader.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				response.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 }
